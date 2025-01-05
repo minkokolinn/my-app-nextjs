5603bf4b8df64b78e16514320b40835aed9d0b80
minkokolinn <minkokolinndev@gmail.com>
2025-01-05 19:24:44 +0700

    add cat image

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello Mister!</h1>
+      <img src="/cat.png"/>
     </div>
   );
 }
